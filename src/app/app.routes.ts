ab5b88c2d03ece31879de97ec5afc25ae27aafd4
Lenapro <helenapoaty7@gmail.com>
2026-04-08 16:57:24 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Competences } from './competences/competences';
+import { Projets } from './projets/projets';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'competences', component: Competences },
+  { path: 'projets', component: Projets },
+  { path: 'contact', component: Contact }
+];
